1e3e6e504b59607c07b5af61c0c4c72708b1dca9
Lucas Carlson <lucas@rufy.com>
2014-02-12 10:01:10 -0600

    Adding serf event management

---
+++ serf-member-leave.sh
@@ -0,0 +1,11 @@
+if [ "x\${SERF_SELF_ROLE}" != "xlb" ]; then
+    echo "Not an lb. Ignoring member leave"
+    exit 0
+fi
+
+while read line; do
+    NAME=\`echo \$line | awk '{print \\\$1 }'\`
+    sed -i'' "/\${NAME} /d" /etc/haproxy/haproxy.cfg
+done
+
+/etc/init.d/haproxy reload
--- start-serf.sh
+++ start-serf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec serf agent -tag role=lb -event-handler=/serf-event-handler.sh
+exec serf agent -tag role=lb -event-handler="member-join=/serf-member-join.sh" -event-handler="member-leave,member-failed=/serf-member-leave.sh"
