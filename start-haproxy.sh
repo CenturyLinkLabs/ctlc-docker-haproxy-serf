02b3dcb6e3008b8575252d161d6f2909d5d3690e
Lucas Carlson <lucas@rufy.com>
2014-02-17 12:22:34 -0800

    Adding stats at runtime

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+if [ -z "$STATS_USERNAME" ]; then
+    export STATS_USERNAME="admin"
+fi  
+
+if [ -z "$STATS_URI" ]; then
+    export STATS_URI="/haproxy?stats"
+fi  
+
+if [ -n "$STATS_PASSWORD" ]
+then
+  echo "	stats enable\n" >> /etc/haproxy/haproxy.cfg
+  echo "	stats auth $STATS_USERNAME:$STATS_PASSWORD\n" >> /etc/haproxy/haproxy.cfg
+  echo "	stats uri $STATS_URI\n" >> /etc/haproxy/haproxy.cfg
+fi
+
 service haproxy start
 exec yes > /dev/null
